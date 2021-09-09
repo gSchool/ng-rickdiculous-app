9ef237a9ff288affbea910b21e69b9aa03732571
Kay <kay.hudson@galvanize.com>
2021-09-09 13:30:20 -0700

    Add loading bar for details.

--- src/app/characters/character-detail/character-detail.component.spec.ts
+++ src/app/characters/character-detail/character-detail.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { CharacterDetailComponent } from './character-detail.component';
 import { CharactersService } from '../../services/characters.service';
 import MockCharactersService from '../../services/mock-characters.service';
+import {Character} from '../../models/character';
 
 describe('CharacterDetailComponent', () => {
   let component: CharacterDetailComponent;
@@ -14,20 +15,26 @@ describe('CharacterDetailComponent', () => {
       providers: [ { provide: CharactersService, useClass: MockCharactersService }]
     })
     .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(CharacterDetailComponent);
     component = fixture.componentInstance;
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   it('should render character', () => {
+    component.character = new Character();
+    component.character.name = 'Jerry';
     fixture.detectChanges();
     const element = fixture.nativeElement.querySelector('.character');
     expect(element.textContent).toContain(component.character.name);
+  });
+
+  it('should render loading bar if no character', () => {
+    component.character = null;
+    fixture.detectChanges();
+    const loadingEl = fixture.nativeElement.querySelector('progress');
+    expect(loadingEl).not.toBeNull();
   });
 });
